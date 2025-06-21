ed4a0c81938b844d860d269bfcacb8d321bcbf45
Blaise <blazzze0517@gmail.com>
2025-06-20 21:19:18 -0400

    Purchase table added with needed relationships.

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Dog(Base):
     owner = relationship('Human', back_populates = 'dog')
 
     def __repr__(self):
-        return f'<Dog ID: {self.id}, Name: {self.name}, Breed: {self.breed}>'
+        return f'<Dog(ID: {self.id}, Name: {self.name}, Breed: {self.breed})>'
 
 
 class Human(Base):
@@ -26,9 +26,22 @@ class Human(Base):
     name = Column(String)
     phone = Column (String)
     dog = relationship('Dog', back_populates = 'owner')
+    purchase = relationship('Purchase', back_populates = 'member')
 
     def __repr__(self):
-        return f'<Owner ID: {self.id}, Name: {self.name}, Dog ID: {self.dog_id}, Phone: {self.phone}>'
+        return f'<Owner(ID: {self.id}, Dog ID: {self.dog_id}, Name: {self.name}, Phone: {self.phone})>'
+
+
+class Purchase(Base):
+    __tablename__ = 'Purchases'
+    id = Column(Integer, primary_key=True)
+    human_id = Column(Integer, ForeignKey('Owner.id'))
+    item = Column(String)
+    price = Column(Integer)
+    member = relationship('Human', back_populates = 'purchase')
+
+    def __repr__(self):
+        return f'<Purchase(ID: {self.id}, Member ID: {self.human_id}, Price: {self.price})>'
 
 
 if __name__ == '__main__':
