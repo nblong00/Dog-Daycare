9473b643320da22bd73558a0269254ec8f7e9704
Blaise <blazzze0517@gmail.com>
2025-06-20 21:43:52 -0400

    Switched single quotes to double quotes for consistency across projects. Also made small formatting changes.

--- models.py
+++ models.py
@@ -2,59 +2,60 @@ from sqlalchemy import (create_engine, Column,
                         Integer, ForeignKey, String)
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
-engine = create_engine('sqlite:///daycare.db', echo = False)
+engine = create_engine("sqlite:///daycare.db", echo = False)
 Session = sessionmaker(bind = engine)
 session = Session()
 Base = declarative_base()
 
 
 class Dog(Base):
-    __tablename__= 'Dogs'
+    __tablename__= "Dogs"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     breed = Column(String)
-    owner = relationship('Human', back_populates = 'dog')
+    owner = relationship("Human", back_populates = "dog")
 
     def __repr__(self):
-        return f'<Dog(ID: {self.id}, Name: {self.name}, Breed: {self.breed})>'
+        return f"<Dog(ID: {self.id}, Name: {self.name}, Breed: {self.breed})>"
 
 
 class Human(Base):
-    __tablename__ = 'Owner'
+    __tablename__ = "Owner"
     id = Column(Integer, primary_key=True)
-    dog_id = Column(Integer, ForeignKey('Dogs.id'))
+    dog_id = Column(Integer, ForeignKey("Dogs.id"))
     name = Column(String)
     phone = Column (String)
-    dog = relationship('Dog', back_populates = 'owner')
-    purchase = relationship('Purchase', back_populates = 'person')
-    subscription = relationship('Subscription', back_populates = 'member')
+    dog = relationship("Dog", back_populates = "owner")
+    purchase = relationship("Purchase", back_populates = "person")
+    subscription = relationship("Subscription", back_populates = "member")
 
     def __repr__(self):
-        return f'<Owner(ID: {self.id}, Dog ID: {self.dog_id}, Name: {self.name}, Phone: {self.phone})>'
+        return f"<Owner(ID: {self.id}, Dog ID: {self.dog_id}, Name: {self.name}, Phone: {self.phone})>"
 
 
 class Purchase(Base):
-    __tablename__ = 'Purchases'
+    __tablename__ = "Purchases"
     id = Column(Integer, primary_key=True)
-    human_id = Column(Integer, ForeignKey('Owner.id'))
+    human_id = Column(Integer, ForeignKey("Owner.id"))
     item = Column(String)
     price = Column(Integer)
-    person = relationship('Human', back_populates = 'purchase')
+    person = relationship("Human", back_populates = "purchase")
 
     def __repr__(self):
-        return f'<Purchase(ID: {self.id}, Person ID: {self.human_id}, Price: {self.price})>'
+        return f"<Purchase(ID: {self.id}, Person ID: {self.human_id}, Price: {self.price})>"
 
 
 class Subscription(Base):
-    __tablename__ = 'Subscriptions'
+    __tablename__ = "Subscriptions"
     id = Column(Integer, primary_key=True)
-    member_id = Column(Integer, ForeignKey('Owner.id'))
+    member_id = Column(Integer, ForeignKey("Owner.id"))
     tier = Column(Integer)
     status = Column(String)
-    member = relationship('Human', back_populates = 'subscription')
+    member = relationship("Human", back_populates = "subscription")
 
     def __repr__(self):
-        return f'<Subscription(ID: {self.id}, Member ID: {self.member_id}, Tier: {self.tier}, Status: {self.status})>'
+        return f"<Subscription(ID: {self.id}, Member ID: {self.member_id}, Tier: {self.tier}, Status: {self.status})>"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     Base.metadata.create_all(engine)
