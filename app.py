b7b24e9af887fdbf052f161d9f9acec68fd591c1
Blaise <blazzze0517@gmail.com>
2025-06-22 15:02:42 -0400

    Setup menu function.

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from models import (Base, engine, session,
                     Dog, Human, Purchase, Subscription)
 
+
+def menu():
+    print('''
+          \n-Dog Daycare Admin Program-
+          \rChoose one of the below options:
+          \r1) Create new Dog & Owner record
+          \r2) Log new Purchase from Owner
+          \r3) Check current Subscriptions
+          \r4) Create new Owner Subscription record
+          \r5) Edit existing Subscription record
+          ''')
+    menu_choice = input("\n> ")
+    while True:
+        if menu_choice not in ["1", "2", "3", "4", "5"]:
+            return menu_choice
+        else:
+            print('''
+                  Invalid entry. Enter one of the below options:
+                  \rChoose one of the below options:
+                  \r1) Create new Dog & Owner record
+                  \r2) Log new Purchase from Owner
+                  \r3) Check current Subscriptions
+                  \r4) Create new Owner Subscription record
+                  \r5) Edit existing Subscription record
+                  ''')
+
+
+
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
