73d6deebfa0451cf0a96816f70e29a48560af3ff
Blaise <blazzze0517@gmail.com>
2025-06-23 13:42:29 -0400

    Fixed issue with menu formatting & inifinite loop in while statement.

--- app.py
+++ app.py
@@ -14,21 +14,20 @@ def menu():
           ''')
     menu_choice = input("\n> ")
     while True:
-        if menu_choice not in ["1", "2", "3", "4", "5"]:
+        if menu_choice in ["1", "2", "3", "4", "5"]:
             return menu_choice
         else:
             print('''
-                  Invalid entry. Enter one of the below options:
-                  \rChoose one of the below options:
+                  \rInvalid entry. Enter one of the below numbered options:
                   \r1) Create new Dog & Owner record
                   \r2) Log new Purchase from Owner
                   \r3) Check current Subscriptions
                   \r4) Create new Owner Subscription record
                   \r5) Edit existing Subscription record
                   ''')
-
-
+            menu_choice = input("\n> ")
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+    menu()
