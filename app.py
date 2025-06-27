7c15bcd81a410e2da98a4b1d45da56adea3767d1
Blaise <blazzze0517@gmail.com>
2025-06-26 21:40:21 -0400

    Now asking for user input to create new dog/owner record. Checks if dog input from user exists already in db.

--- app.py
+++ app.py
@@ -30,20 +30,35 @@ def menu():
             menu_choice = input("> ")
 
 
-    def app(menu_choice):
-        if menu_choice == "1":
-            pass
-        elif menu_choice == "2":
-            pass
-        elif menu_choice == "3":
-            pass
-        elif menu_choice == "4":
-            pass
-        elif menu_choice == "5":
-            pass
-        elif menu_choice == "6":
-            exit()
+def app(menu_choice):
+    if menu_choice == "1":
+        while True:
+            dog_name = input("Enter dog's name: ")
+            dog_breed = input("Enter dog's breed: ")
+            owner_name = input("Enter owner's name: ")
+            owner_phone = input("Enter owner's phone number: ")
+            animal_check = session.query(Dog).filter(Dog.name==dog_name).filter(Dog.breed==dog_breed).first()
+            if animal_check == None:
+                pass
+            else:
+                print("\nDog and breed combo already exists in database.")
+                print("Enter info for dog not currently in system.")
+                continue
+
+    elif menu_choice == "2":
+        pass
+    elif menu_choice == "3":
+        pass
+    elif menu_choice == "4":
+        pass
+    elif menu_choice == "5":
+        pass
+    elif menu_choice == "6":
+        exit()
+    else:
+        pass
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     menu_choice = menu()
+    app(menu_choice)
