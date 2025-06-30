57017b5083f638d34da54ca00533dcfbfea8c11c
Blaise <blazzze0517@gmail.com>
2025-06-29 21:59:36 -0400

    Adjusted menu to cut down on \n usage and cutdown on repetitive code.

--- app.py
+++ app.py
@@ -4,30 +4,23 @@ import time
 
 
 def menu():
-    print('''
-          \n-Dog Daycare Admin Program-
-          \rChoose one of the below options:
-          \r1) Create new Dog & Owner record
-          \r2) Log new Purchase from Owner
-          \r3) Check current Subscriptions
-          \r4) Create new Owner Subscription record
-          \r5) Edit existing Subscription record
-          \r6) EXIT PROGRAM
-          ''')
+    menu =  '''
+            \r-Dog Daycare Admin Program-
+            \rChoose one of the below options:
+            \r1) Create new Dog & Owner record
+            \r2) Log new Purchase from Owner
+            \r3) Check current Subscriptions
+            \r4) Create new Owner Subscription record
+            \r5) Edit existing Subscription record
+            \r6) EXIT PROGRAM
+            '''
+    print(menu)
     menu_choice = input("> ")
     while True:
         if menu_choice in ["1", "2", "3", "4", "5", "6"]:
             return menu_choice
         else:
-            print('''
-                  \rInvalid entry. Enter one of the below numbered options:
-                  \r1) Create new Dog & Owner record
-                  \r2) Log new Purchase from Owner
-                  \r3) Check current Subscriptions
-                  \r4) Create new Owner Subscription record
-                  \r5) Edit existing Subscription record
-                  \r6) EXIT PROGRAM
-                  ''')
+            print(menu)
             menu_choice = input("> ")
 
 
