51e66463a42d592b18b098fb87f9d9530342820f
Blaise <blazzze0517@gmail.com>
2025-07-11 14:12:00 -0400

    Dog/owner specific info now in separate function.

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ def menu():
             menu_choice = input("> ")
 
 
+def dog_info():
+    dog_name = input("Enter dog's name: ")
+    dog_breed = input("Enter dog's breed: ")
+    owner_name = input("Enter owner's name: ")
+    owner_phone = input("Enter owner's phone number: ")
+    return dog_name, dog_breed, owner_name, owner_phone
+
+
 def purchase_convert():
     purchase = float(input("Enter purchase total (ex. 19.99): "))
     purchase_pennies = int(purchase * 100)
@@ -48,10 +56,8 @@ def create_dog_owner(dog_name, dog_breed,
             else:
                 print("\nDog and breed combo already exists in database.")
                 print("Enter info for dog not currently in system.\n")
-                dog_name = input("Enter dog's name: ")
-                dog_breed = input("Enter dog's breed: ")
-                owner_name = input("Enter owner's name: ")
-                owner_phone = input("Enter owner's phone number: ")
+                (dog_name, dog_breed,
+                owner_name, owner_phone) = dog_info()
                 continue
 
 
@@ -101,10 +107,8 @@ def create_owner_sub():
             print("* Enter EXIT to return to Main Menu.")
             missing_item = input("\n> ")
             if missing_item in ["yes", "ye", "y"]:
-                dog_name = input("Enter dog's name: ")
-                dog_breed = input("Enter dog's breed: ")
-                owner_name = input("Enter owner's name: ")
-                owner_phone = input("Enter owner's phone number: ")
+                (dog_name, dog_breed,
+                owner_name, owner_phone) = dog_info()
                 animal_check = session.query(Dog).filter(Dog.name == dog_name).filter(Dog.breed == dog_breed).first()
                 phone_check = session.query(Human).filter(Human.phone == owner_phone).first()
                 if animal_check == None:
@@ -227,10 +231,8 @@ def app():
     while True:
         menu_choice = menu()
         if menu_choice == "1":
-            dog_name = input("Enter dog's name: ")
-            dog_breed = input("Enter dog's breed: ")
-            owner_name = input("Enter owner's name: ")
-            owner_phone = input("Enter owner's phone number: ")
+            (dog_name, dog_breed,
+            owner_name, owner_phone) = dog_info()
             create_dog_owner(dog_name, dog_breed,
                              owner_name, owner_phone)
         elif menu_choice == "2":
